c882586f674d8302cb19da752e18630840cc2842
Dam Anh Dien <2551050029dien@ou.edu.vn>
2026-08-22 20:58:50 +0700

    them thanh cuon len dau trang

---
+++ js/main.js
@@ -0,0 +1,15 @@
+import { initToTop } from "./nav.js";
+//import { initTheme } from "./theme.js";
+//import { initFaq } from "./faq.js";
+//import { initPricing } from "./pricing.js";
+//import { initSlider } from "./slider.js";
+//import { initReveal } from "./reveal.js";
+
+//initNav();
+//initHeaderOnScroll();
+initToTop();
+//initTheme();
+//initFaq();
+//initPricing();
+//initSlider();
+//initReveal();
